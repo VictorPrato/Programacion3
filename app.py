889a26841372660f5585713ecc4cd14ca3263321
VictorPrato <pratoovallesvictormanuel@gmail.com>
2026-02-13 12:12:37 -0400

    arreglos de server

--- app.py
+++ app.py
@@ -183,4 +183,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Esto permite que Render asigne el puerto y que la app sea visible (0.0.0.0)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
